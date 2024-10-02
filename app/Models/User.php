073f8c29e7b75cce06abaf02e3ce2e838893c239
Renato <rverissimo.dev@gmail.com>
2024-10-02 16:33:45 -0300

    Criando notificações customizadas para as mensagens de email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\RedifinirSenhaNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -37,4 +38,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new RedifinirSenhaNotification($token));
+    }
 }
