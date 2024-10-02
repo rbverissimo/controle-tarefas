78a7f89f754fbb82424f5536ba8cae380ea66ac8
Renato <rverissimo.dev@gmail.com>
2024-10-02 14:01:19 -0300

    Aula bastante longa em que entendi como publicar os emails através de php artisan vendor:publish passando o mail para o parâmetro. Essa publicação cria os componentes que formam o template do e-mail e assim é possível editar a estrutura do e-mail. Também entendi um pouco do peso da variável de ambiente APP_NAME.

--- resources/views/emails/mensagem-teste.blade.php
+++ resources/views/emails/mensagem-teste.blade.php
@@ -3,9 +3,18 @@
 
 corpo da mensagem
 
+- Opção 1
+- Opção 2
+
 @component('mail::button', ['url' => ''])
 Ok
 @endcomponent
+
+
+@component('mail::button', ['url' => ''])
+Não ok
+@endcomponent
+
 
 Obrigado,<br>
 {{ config('app.name') }}
